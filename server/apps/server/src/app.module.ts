4f1f417474dd31f3cc8f64755590c9a3662089c1
CuberLuo <1501858187@qq.com>
2026-05-04 13:42:56 +0800

    feat(word-book): 添加单词本功能模块
    实现单词本功能，包括：
    - 添加@en/common依赖
    - 创建单词本模块、服务和控制器
    - 实现单词查询接口
    - 添加前端单词本页面和API调用
    - 配置API代理和版本控制
    - 添加发音功能hook

--- server/apps/server/src/app.module.ts
+++ server/apps/server/src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { WordBookModule } from './word-book/word-book.module';
 import { SharedModule } from '@libs/shared';
 
 @Module({
-  imports: [UserModule, SharedModule],
+  imports: [UserModule, WordBookModule, SharedModule],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService],
 })
 export class AppModule {}
