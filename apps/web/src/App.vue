3436b4890916b9930ffe60b564eb850e576d18f3
CuberLuo <1501858187@qq.com>
2026-05-05 22:00:19 +0800

    feat(搜索): 添加全局搜索组件和自动聚焦指令
    实现全局搜索功能，包括：
    1. 添加Search组件支持Ctrl+F快捷键触发
    2. 创建v-focus指令实现输入框自动聚焦
    3. 支持搜索结果显示和内容复制
    4. 添加过渡动画效果

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <RouterView />
+  <Search />
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import Search from './components/Search/index.vue'
 </script>
 
 <style scoped></style>
