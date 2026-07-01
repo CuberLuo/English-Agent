053476eafa9290fe54b33ba3f87ec1d9c2ed387d
CuberLuo <1501858187@qq.com>
2026-07-01 21:24:51 +0800

    feat: 添加课程模块及相关功能
    新增多类英语词汇课程，包含高考、四六级、托福雅思等，搭建前后端课程管理系统，配置prisma种子数据与minio资源存储，完善路由与页面展示

--- server/apps/server/src/app.module.ts
+++ server/apps/server/src/app.module.ts
@@ -6,9 +6,10 @@ import { WordBookModule } from './word-book/word-book.module';
 import { SharedModule } from '@libs/shared';
 import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
+import { CourseModule } from './course/course.module';
 
 @Module({
-  imports: [UserModule, WordBookModule, SharedModule, AuthModule],
+  imports: [UserModule, WordBookModule, SharedModule, AuthModule, CourseModule],
   controllers: [AppController],
   providers: [AppService, AuthService],
 })
